b5bf85a949ea8f64ac710799cd4161ca4bd9c29b
Akenokoru <akenokoru.pan@gmail.com>
2021-02-08 19:41:52 +0800

    feat: add search

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,6 +1,38 @@
+import React from 'react';
+import { useEffect, useState } from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
+import * as qs from 'qs'
+import { cleanObject } from '../../utils'
+
+const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => {
-  return <div></div>
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  }, [param])
+
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam}/>
+    <List list={list} users={users}/>
+  </div>
 }
--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,35 +1,19 @@
-import { useEffect, useState } from "react"
+import React from 'react';
 
-export const SearchPanel = () => {
-  const [param, setParam] = useState({
-    name: '',
-    personId: ''
-  })
-
-  const [users, setUsers] = useState([])
-  const [list, setList] = useState([])
-
-  useEffect(() => {
-    fetch('').then(async response => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
-  }, [param])
-
+export const SearchPanel = ({users, param, setParam}) => {
   return <form action="">
     <div>
       <input type="text" value={param.name} onChange={evt => setParam({
         ...param,
         name: evt.target.value
       })}/>
-      <select value={value.personId} onChange={evt => setParam({
+      <select value={param.personId} onChange={evt => setParam({
         ...param,
         personId: evt.target.value
       })}>
         <option value={''}>负责人</option>
         {
-          users.map(user => <option value={user.name}></option>)
+          users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
         }
       </select>
     </div>
